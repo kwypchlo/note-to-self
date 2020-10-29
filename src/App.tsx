78fb219c0f7eb94fac85d1beec1655995b6982e5
Karol Wypchlo <kwypchlo@gmail.com>
2020-10-29 11:20:52 +0100

    update app to new sdk

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import React, { useState } from "react";
+import { SkynetClient, keyPairFromSeed } from "skynet-js";
 import logo from "./logo.svg";
 import "./App.css";
 import "fontsource-metropolis/all.css";
-import { SkynetClient, SkyFile, FileID, User, FileType } from "skynet-js";
 
-const skynetClient = new SkynetClient("https://siasky.net");
-const filename = "note.txt";
-const fileID = new FileID("note-to-self", FileType.PublicUnencrypted, filename);
+const skynetClient = new SkynetClient("https://siasky.dev");
+const filename = "data.json";
 
 function App() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [secret, setSecret] = useState("");
   const [note, setNote] = useState("");
+  const [revision, setRevision] = useState(0);
   const [noteExists, setNoteExists] = useState(false);
   const [authenticated, setAuthenticated] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
@@ -19,18 +18,14 @@ function App() {
   const [displaySuccess, setDisplaySuccess] = useState(false);
   const loadNote = async () => {
     try {
-      const user = new User(username, password);
-      const skyFile = await skynetClient.getFile(user, fileID);
-      const reader = new FileReader();
-      reader.onload = () => {
-        setNote(reader.result as string);
+      const { publicKey } = keyPairFromSeed(secret);
+      const entry = await skynetClient.db.getJSON(publicKey, filename);
+
+      if (entry) {
+        setNote(entry.data?.note ?? "");
+        setRevision(entry.revision);
         setNoteExists(true);
-      };
-      reader.onerror = () => {
-        setNote("");
-        setErrorMessage(`Failed to read file: ${reader.error}`);
-      };
-      reader.readAsText(skyFile.file);
+      }
     } catch (error) {
       setErrorMessage(error.message);
       setNote("");
@@ -48,13 +43,16 @@ function App() {
   const handleSetNote = async () => {
     setLoading(true);
 
-    const file = new File([note], filename, { type: "text/plain" });
-    const skyfile = new SkyFile(file);
-    const user = new User(username, password);
+    const { privateKey } = keyPairFromSeed(secret);
     try {
-      await skynetClient.setFile(user, fileID, skyfile);
-      await loadNote();
+      await skynetClient.db.setJSON(
+        privateKey,
+        filename,
+        { note },
+        revision + 1
+      );
 
+      setRevision(revision + 1);
       setDisplaySuccess(true);
       setTimeout(() => setDisplaySuccess(false), 5000);
     } catch (error) {
@@ -98,35 +96,21 @@ function App() {
                   {loading ? "Sending..." : "Save this note"}
                 </button>
                 {displaySuccess && (
-                  <span className="success-message">
-                    Your note has been saved!
-                  </span>
+                  <span className="success-message">Note saved!</span>
                 )}
               </div>
             </div>
           ) : (
             <form onSubmit={handleLogin}>
               <div className="mb-2">
-                <label htmlFor="input">Login</label>
-                <div className="flex">
-                  <input
-                    id="input"
-                    type="text"
-                    placeholder="Your username"
-                    value={username}
-                    onChange={(event) => setUsername(event.target.value)}
-                  />
-                </div>
-              </div>
-              <div className="mb-2">
-                <label htmlFor="output">Password</label>
+                <label htmlFor="output">Secret</label>
                 <div className="flex">
                   <input
                     id="output"
-                    type="password"
-                    placeholder="Your password"
-                    value={password}
-                    onChange={(event) => setPassword(event.target.value)}
+                    type="secret"
+                    placeholder="Your very secret passphrase"
+                    value={secret}
+                    onChange={(event) => setSecret(event.target.value)}
                   />
                 </div>
                 <div>
@@ -135,7 +119,7 @@ function App() {
               </div>
               <div className="mb-4">
                 <button disabled={loading}>
-                  {loading ? "Logging in..." : "Log in"}
+                  {loading ? "Loading..." : "Load"}
                 </button>
               </div>
             </form>
